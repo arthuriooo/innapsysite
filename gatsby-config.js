a29919084c1abc42449cebd133e096d2d37af980
arthuriooo <arthuriooo@yandex.ru>
2019-10-19 22:27:17 +0300

    fix: fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,13 +4,6 @@ module.exports = {
     description: 'Психолог, психоаналитик, сексолог - доступ к счастью тут.',
   },
   plugins: [
-    // `gatsby-transformer-remark`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/pages`
-      }
-    }
+    'gatsby-plugin-react-helmet'
   ],
 }
