bb1b8f1cbc22c56ce14712b560a14af3147af697
Arti <arthuriooo@yandex.ru>
2019-10-19 21:55:41 +0300

    fix: first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Психолог Инна',
+    description: 'Психолог, психоаналитик, сексолог - доступ к счастью тут.',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      }
+    }
+  ],
 }
